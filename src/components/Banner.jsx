92dda25b9ace0ec0c405c8fa35cdc9feeda103bc
Alan Webb <webbalan91939@gmail.com>
2026-05-05 11:43:12 +0100

    Update Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,20 @@
+import {useState} from "react";
+
 const Banner = () => {
-	return <div className="bg-white pt-12 md:pt-24 min-h-125">
-		{/* VIDEO MODAL */}
-		
-	</div>;
+	const [showVideo, setShowVideo] = useState(false);
+
+	return (
+		<div className="bg-white pt-12 md:pt-24 min-h-125">
+			{/* VIDEO MODAL */}
+			<div className="fixed inset-o bg-black/50 z-50 flex items-center justify-center p-4">
+				<div className="bg-white p-4 md:p-8 rounded-lg w-full max-4xl relative">
+					<button
+						onClick={() => setShowVideo(false)}
+						className="absolute -top-8 right-0 text-white text-2xl"></button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
