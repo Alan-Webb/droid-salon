98428aeb0a4f69426b93c03de9d6c0ecaa164259
Alan Webb <webbalan91939@gmail.com>
2026-05-22 08:30:56 +0100

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,16 @@ const Hero = () => {
 			id="home"
 			className="min-h-[80vh] md:min-h-[90vh] flex items-center justify-center text-center px-4 relative overflow-hidden"
 			style={{
-				background: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.8)), url(${heroImage})`,
+				background: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.4)), url(${heroImage})`,
 				backgroundSize: `cover`,
 				backgroundPosition: `center`,
 			}}>
 			<div className="max-w-6xl text-white z-10 relative px-4">
-				<div className="absolute -bottom-12 -right-12 md:-bottom-8 md:-right-8 text-4xl md:text-5xl text-amber-300/20 rotate-12"><GiRobotGolem /></div>
+				<div className="absolute -bottom-12 -right-12 md:-bottom-8 md:-right-8 text-4xl md:text-5xl text-amber-300/90 rotate-12"><GiRobotGolem /></div>
+				{/* HEADING */}
+				<h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 md:mb-6 bg-linear-to-r from-amber-300 via-rose-300 to-rose-500 bg-clip-text text-transparent drop-shadow-2xl animate-fade-in">
+					Droid Salon
+				</h1>
 			</div>
 		</div>
 	);
