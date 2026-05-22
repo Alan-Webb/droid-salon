a5f47e126675ce921bd5aad980b77d32f7b6bbd8
Alan Webb <webbalan91939@gmail.com>
2026-05-22 08:11:03 +0100

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import heroImage from "../assets/heroImage.webp";
+import { GiRobotGolem } from "react-icons/gi";
 
 const Hero = () => {
 	return (
@@ -9,7 +10,11 @@ const Hero = () => {
 				background: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.8)), url(${heroImage})`,
 				backgroundSize: `cover`,
 				backgroundPosition: `center`,
-			}}></div>
+			}}>
+			<div className="max-w-6xl text-white z-10 relative px-4">
+				<div className="absolute -bottom-12 -right-12 md:-bottom-8 md:-right-8 text-4xl md:text-5xl text-amber-300/20 rotate-12"><GiRobotGolem /></div>
+			</div>
+		</div>
 	);
 };
 
