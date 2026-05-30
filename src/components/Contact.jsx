65a2cf9fac40e12427b093cfd0dc7124253e4140
Alan Webb <webbalan91939@gmail.com>
2026-05-30 13:09:04 +0100

    Create & update Contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+const Contact = () => {
+	return (
+		<section
+			id="contact"
+			className="py-20 bg-yellow-50 min-h-screen px-4 sm:px-6 lg:px-8">
+			<div className="container mx-auto max-w-2xl">
+				<div className="text-center mb-12">
+					<h2 className="text-4xl sm:text-5xl font-bold mb-4 bg-linear-to-r from-amber-300 via-rose-300 to-amber-500 bg-clip-text text-transparent">
+						Book An Appointment
+					</h2>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Contact;
