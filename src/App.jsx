86e8136615f155fa989bfddf52535daf0ea2acb0
Alan Webb <webbalan91939@gmail.com>
2026-06-11 10:40:20 +0100

    Create & update Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
@@ -12,6 +13,7 @@ const App = () => {
 			<About />
 			<Services />
 			<Contact />
+			<Footer />
 		</>
 	);
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+	return (
+		<footer className="bg-white border-t border-gray-100">
+			<div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-12">
+          {/* BRAND SECTION */}
+          <div className="text-center md:text-left lg:text-center xl:text-left">
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+		</footer>
+	);
+};
+
+export default Footer;
