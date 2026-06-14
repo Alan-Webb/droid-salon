6902c9394c7af9059c313f82d5ca0f1c2ff05082
Alan Webb <webbalan91939@gmail.com>
2026-06-14 07:06:52 +0100

    Update Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.webp";
+
+const Navbar = () => {
+	return (
+		<div>
+			<div>
+				<div>
+					<img src={logo} alt="Droid Charging Battery" className="w-32 h-32" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
