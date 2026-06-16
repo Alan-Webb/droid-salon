03fb9e37459d2efb0c8abb52afda565e7139508e
Alan Webb <webbalan91939@gmail.com>
2026-06-16 02:00:27 +0100

    Update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,24 @@ const Navbar = () => {
 	return (
 		<div>
 			<div>
+				{/* LOGO */}
 				<div>
 					<img src={logo} alt="Droid Charging Battery" className="w-32 h-32" />
+				</div>
+				{/* CONTACT DETAILS */}
+				<div>
+					<div>
+						<div>email</div>
+						<div>00000000</div>
+					</div>
+				</div>
+				{/* NAVIGATION */}
+				<div>
+					<a href="#">Home</a>
+					<a href="#">Home</a>
+					<a href="#">Home</a>
+					<a href="#">Home</a>
+					<a href="#">Home</a>
 				</div>
 			</div>
 		</div>
