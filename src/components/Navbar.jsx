be59d2131c27a39df0f7f3b6137a3283b5b71c36
Alan Webb <webbalan91939@gmail.com>
2026-06-16 02:16:52 +0100

    Update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,40 @@ import logo from "../assets/logo.webp";
 
 const Navbar = () => {
 	return (
-		<div>
-			<div>
-				{/* LOGO */}
+		<div className="bg-gray-200">
+			<div className="flex justify-between items-center max-w-7xl mx-auto">
 				<div>
-					<img src={logo} alt="Droid Charging Battery" className="w-32 h-32" />
+					{/* LOGO */}
+					<div>
+						<img
+							src={logo}
+							alt="Droid Charging Battery"
+							className="w-20 h-20 m-1 rounded-full"
+						/>
+					</div>
 				</div>
 				{/* CONTACT DETAILS */}
-				<div>
-					<div>
+				<div className="flex flex-col">
+					<div className="flex justify-end">
 						<div>email</div>
 						<div>00000000</div>
 					</div>
+					{/* NAVIGATION */}
+					<div className="flex space-x-4">
+						<a href="#">Home</a>
+						<a href="#">Home</a>
+						<a href="#">Home</a>
+						<a href="#">Home</a>
+						<a href="#">Home</a>
+						<a href="#">Home</a>
+						<a href="#">Home</a>
+					</div>
 				</div>
-				{/* NAVIGATION */}
+				{/* BUTTON */}
 				<div>
-					<a href="#">Home</a>
-					<a href="#">Home</a>
-					<a href="#">Home</a>
-					<a href="#">Home</a>
-					<a href="#">Home</a>
+					<button className="px-5 py-2 bg-sky-500 text-white rounded-full hover:bg-sky-600 cursor-pointer">
+						Get A Quote
+					</button>
 				</div>
 			</div>
 		</div>
