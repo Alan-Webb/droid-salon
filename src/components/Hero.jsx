bb515bf5ae397ac62c3f0e180c8fdcfc0581a1ce
Alan Webb <webbalan91939@gmail.com>
2026-06-26 02:15:55 +0100

    Update assets & Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,25 @@
+import Hero1 from "../assets/hero1.webp";
+
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<div>
+			<div className="relative">
+				{/* IMAGE CAROUSEL */}
+				<div>
+					<img src={Hero1} alt="multi-purpose droids" />
+					{/* Overlay */}
+					<div className="absolute inset-0 bg-linear-to-r from-black via-amber-900/50 to-amber-800/50"></div>
+				</div>
+				{/* CONTENT */}
+				<div>
+					<div>
+						<h1>Droid Salon</h1>
+						<p>Your Droids Best Version Starts Here</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
