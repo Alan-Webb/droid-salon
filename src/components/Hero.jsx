3d61800259a2513d0dfe02365ea2a3aef43b3c3d
Alan Webb <webbalan91939@gmail.com>
2026-06-28 06:28:49 +0100

    Update Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,13 @@ const Hero = () => {
 		<div>
 			<div className="relative">
 				{/* IMAGE CAROUSEL */}
-				<div>
+				<div className="relative">
 					<img src={Hero1} alt="multi-purpose droids" />
 					{/* Overlay */}
 					<div className="absolute inset-0 bg-linear-to-r from-black via-amber-900/30 to-amber-800/30"></div>
 				</div>
 				{/* CONTENT */}
-				<div className="absolute">
+				<div className="absolute inset-0 flex items-center justify-start max-w-7xl mx-auto text-4xl text-amber-400">
 					<div>
 						<h1>Droid Salon</h1>
 						<p>Your Droids Best Version Starts Here</p>
