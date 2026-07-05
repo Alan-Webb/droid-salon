31d9270cd121192a7ef333ac458537beafcefa8c
Alan Webb <webbalan91939@gmail.com>
2026-07-05 07:27:30 +0100

    Update WhyChooseUs section

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -4,8 +4,18 @@ const WhyChooseUs = () => {
 			<div className="flex flex-col lg:flex-row">
 				{/* Left Content */}
 				<div>
-					<h2 className="text-3xl font-bold mb-4 px-4 lg:px-0">Why Choose Us?</h2>
-					<p></p>
+					<h2 className="text-3xl font-bold mb-4 px-4 lg:px-0">
+						Why Choose Us?
+					</h2>
+					<p>
+						Combining expert craftsmanship with advanced technology, we deliver
+						reliable maintenance and precision upgrades for every generation of
+						droid.
+						<br />
+						From routine servicing to complete system overhauls, our technicians
+						provide dependable solutions that keep your droid performing at its
+						very best
+					</p>
 				</div>
 				{/* Right Content */}
 				<div></div>
