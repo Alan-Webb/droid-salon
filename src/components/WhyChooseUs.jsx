178fd63b5bc345c452ea17b84ad2cf61452ffc96
Alan Webb <webbalan91939@gmail.com>
2026-07-05 07:17:23 +0100

    Update WhyChooseUs section

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -1,9 +1,12 @@
 const WhyChooseUs = () => {
 	return (
 		<div className="py-20 bg-gray-100">
-			<div className="flex">
+			<div className="flex flex-col lg:flex-row">
 				{/* Left Content */}
-				<div></div>
+				<div>
+					<h2 className="text-3xl font-bold mb-4 px-4 lg:px-0">Why Choose Us?</h2>
+					<p></p>
+				</div>
 				{/* Right Content */}
 				<div></div>
 			</div>
