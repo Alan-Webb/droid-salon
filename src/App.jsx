a5b7a4fb1d15476d55c8612b3321814fc1be8fd2
Alan Webb <webbalan91939@gmail.com>
2026-07-09 01:53:52 +0100

    Update assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import WhyChooseUs from "./components/WhyChooseUs";
+import Services from "./components/Services";
 
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<WhyChooseUs />
+			<Services />
 		</>
 	);
 };
