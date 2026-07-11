3ab7a91e7e958c24bc925ff8aab09d099b018433
Alan Webb <webbalan91939@gmail.com>
2026-07-11 11:36:44 +0100

    Update Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,26 @@
-const Services = () => {
-  return (
-    <div>
-      <div>
-        <h2>Our Services</h2>
-      </div>
-    </div>
-  )
-}
+import Services1 from "../assets/services1.webp";
+import Services2 from "../assets/services2.webp";
+import Services3 from "../assets/services3.webp";
 
-export default Services
+const services = [
+	{
+		id: 1,
+		title: "Combat Upgrades",
+		image: Services1,
+		link: "",
+		description:
+			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, nobis odit",
+	},
+];
+
+const Services = () => {
+	return (
+		<div>
+			<div>
+				<h2>Our Services</h2>
+			</div>
+		</div>
+	);
+};
+
+export default Services;
