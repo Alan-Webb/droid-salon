6ef4bff74cd40100e4d9b5a42c09bfa0e53de928
Alan Webb <webbalan91939@gmail.com>
2026-07-23 10:00:13 +0100

    Update Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,20 @@
-
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+	return (
+		<div>
+			<div>
+				{/* CONTACT DETAILS */}
+				<div>
+					<h2>Contact Us</h2>
+					<p>
+						Fine tuning, software updates or a complete overhaul, contact us to
+						upgrade your droid and upgrade your life{" "}
+					</p>
+				</div>
+				{/* FORM */}
+				<div></div>
+			</div>
+		</div>
+	);
+};
 
-export default Contact
+export default Contact;
