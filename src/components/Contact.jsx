d031eaf70c7282d820f26cb3f1e07769a34903fc
Alan Webb <webbalan91939@gmail.com>
2026-07-24 08:13:47 +0100

    Update Contact comp & create Form.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,20 @@ const Contact = () => {
 	return (
 		<div>
 			<div>
-				{/* CONTACT DETAILS */}
+				{/* LEFT SIDE CONTENT */}
 				<div>
 					<h2>Contact Us</h2>
 					<p>
-						Fine tuning, software updates or a complete overhaul, contact us to
-						upgrade your droid and upgrade your life.
+						Software updates, battery replacement or a complete overhaul, our
+						engineers will give you expert advice on getting your droid back to
+						its best. The Droid Salon team is here to help. We pride ourselves
+						on delivering a fast, reliable service, honest pricing, and friendly
+						customer service you can trust. No matter the issue, we'll take the
+						time to explain your options and provide a solution that tailored to
+						your needs. Upgrade your droid and upgrade your life.
 					</p>
 				</div>
-				{/* FORM */}
+				{/* RIGHT SIDE FORM */}
 				<div></div>
 			</div>
 		</div>
---
+++ src/components/Form.jsx
@@ -0,0 +1,9 @@
+const Form = () => {
+	return (
+		<form className="grid grid-cols-1 md:grid-cols-2 space-y-2">
+			<div></div>
+		</form>
+	);
+};
+
+export default Form;
