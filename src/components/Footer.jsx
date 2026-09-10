90cf4ae810d2a688a8f562657d87fb70b9dfbb74
Alan Webb <webbalan91939@gmail.com>
2026-09-10 09:57:33 +0100

    Update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="py-8 bg-stone-400">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4"></div>
+		</div>
+	);
 };
 
 export default Footer;
