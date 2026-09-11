84f24e9942b2168cf311b4b3e67cdf780b4e125f
Alan Webb <webbalan91939@gmail.com>
2026-09-11 06:53:01 +0100

    Update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ const Footer = () => {
 		<div className="py-8 bg-stone-400">
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
 				{/* COLUMN 1 CONTENT */}
-				<div></div>
+				<div>
+					<img/>
+				</div>
 				{/* COLUMN 2 CONTENT */}
 				<div></div>
 				{/* COLUMN 3 CONTENT */}
