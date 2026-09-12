b3569005bee0ea95cd51414d2e509486b5156292
Alan Webb <webbalan91939@gmail.com>
2026-09-12 05:18:52 +0100

    Update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import logo from "../assets/logo.webp";
+
 const Footer = () => {
 	return (
 		<div className="py-8 bg-stone-400">
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
 				{/* COLUMN 1 CONTENT */}
 				<div>
-					<img src="../assets/icons8-robot-48.png" />
+					<img
+						src={logo}
+						alt="Droid Salon Logo"
+						className="w-12 lg:w-20 h-12 lg:h-20 m-2 rounded-full bg-amber-600 border border-white"
+					/>
 				</div>
 				{/* COLUMN 2 CONTENT */}
 				<div></div>
